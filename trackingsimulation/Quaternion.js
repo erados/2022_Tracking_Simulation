5b2da1302bec81ffc8d4297e4ff02eb74b19f6de
erados <imdiots@gmail.com>
2022-11-07 12:57:52 +0900

    formatting

--- trackingsimulation/Quaternion.js
+++ trackingsimulation/Quaternion.js
@@ -1,46 +1,50 @@
 //var matrix = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
-var matrix = [[0.730415003950535, 0.008259709357830162, 0.6829536581681568], [0.3025458868184332, 0.8925611986512936, -0.3343658072103259], [-0.6123397001446084, 0.450850622560831, 0.6494411503464521]];
-function Quaternion(w, point){
-	this.w = w;
-	this.x = point.x;
-	this.y = point.y;
-	this.z = point.z;
+var matrix = [
+  [0.730415003950535, 0.008259709357830162, 0.6829536581681568],
+  [0.3025458868184332, 0.8925611986512936, -0.3343658072103259],
+  [-0.6123397001446084, 0.450850622560831, 0.6494411503464521],
+];
+function Quaternion(w, point) {
+  this.w = w;
+  this.x = point.x;
+  this.y = point.y;
+  this.z = point.z;
 
-	return this;
+  return this;
 }
 
-Quaternion.prototype.set_pos = function(w, point){
-	this.w = w;
-	this.x = point.x;
-	this.y = point.y;
-	this.z = point.z;
+Quaternion.prototype.set_pos = function (w, point) {
+  this.w = w;
+  this.x = point.x;
+  this.y = point.y;
+  this.z = point.z;
 
-	return this;
-}
+  return this;
+};
 
-Quaternion.prototype.calculate_rotation_matrix = function(){
-	var matrix = [[],[],[]];
-	var qxx = (this.x * this.x);
-	var qyy = (this.y * this.y);
-	var qzz = (this.z * this.z);
-	var qxz = (this.x * this.z);
-	var qxy = (this.x * this.y);
-	var qyz = (this.y * this.z);
-	var qwx = (this.w * this.x);
-	var qwy = (this.w * this.y);
-	var qwz = (this.w * this.z);
+Quaternion.prototype.calculate_rotation_matrix = function () {
+  var matrix = [[], [], []];
+  var qxx = this.x * this.x;
+  var qyy = this.y * this.y;
+  var qzz = this.z * this.z;
+  var qxz = this.x * this.z;
+  var qxy = this.x * this.y;
+  var qyz = this.y * this.z;
+  var qwx = this.w * this.x;
+  var qwy = this.w * this.y;
+  var qwz = this.w * this.z;
 
-	matrix[0][0] = 1 - 2 * (qyy + qzz);
-	matrix[0][1] = 2 * (qxy + qwz);
-	matrix[0][2] = 2 * (qxz - qwy);
-	
-	matrix[1][0] = 2 * (qxy - qwz);
-	matrix[1][1] = 1 - 2 * (qxx + qzz);
-	matrix[1][2] = 2 * (qyz + qwx);
-	
-	matrix[2][0] = 2 * (qxz + qwy);
-	matrix[2][1] = 2 * (qyz - qwx);
-	matrix[2][2] = 1 - 2 * (qxx + qyy);
+  matrix[0][0] = 1 - 2 * (qyy + qzz);
+  matrix[0][1] = 2 * (qxy + qwz);
+  matrix[0][2] = 2 * (qxz - qwy);
 
-	return matrix;
-}
+  matrix[1][0] = 2 * (qxy - qwz);
+  matrix[1][1] = 1 - 2 * (qxx + qzz);
+  matrix[1][2] = 2 * (qyz + qwx);
+
+  matrix[2][0] = 2 * (qxz + qwy);
+  matrix[2][1] = 2 * (qyz - qwx);
+  matrix[2][2] = 1 - 2 * (qxx + qyy);
+
+  return matrix;
+};
--- trackingsimulation/js3.js
+++ trackingsimulation/js3.js
@@ -2,41 +2,43 @@ var old_pos = new Point(0, 0, 0);
 var new_pos = new Point(0, 0, 0);
 var isdraging = false;
 var axis_vector = new Point(0, 0, 0);
-var theta = Math.PI/180;
+var theta = Math.PI / 180;
 var cos_theta = Math.cos(theta);
 var sin_theta = Math.sin(theta);
-canvas.addEventListener('mousedown', mousedown);
-canvas.addEventListener('mousemove', mousemove);
-canvas.addEventListener('mouseup', mouseup);	
+canvas.addEventListener("mousedown", mousedown);
+canvas.addEventListener("mousemove", mousemove);
+canvas.addEventListener("mouseup", mouseup);
 
-
-function mousedown(e){
-	isdraging = true;
-	x = e.layerX-width/2;
-	y = e.layerY-height/2;
-	z = Math.sqrt(width * width / 4 - x * x - y * y);
-	old_pos.set_pos(x, y, z);
+function mousedown(e) {
+  isdraging = true;
+  x = e.layerX - width / 2;
+  y = e.layerY - height / 2;
+  z = Math.sqrt((width * width) / 4 - x * x - y * y);
+  old_pos.set_pos(x, y, z);
 }
 
-function mousemove(e){
-	if(isdraging){
-		x = e.layerX-width/2;
-		y = e.layerY-height/2;
-		z = Math.sqrt(width * width / 4 - x * x - y * y);
-		new_pos.set_pos(x, y, z);
-		
-		//rotation part here
-		var cp = old_pos.cross_product(new_pos);
-		if(cp.x != 0){
-			axis_vector = cp.nomalize();
-			rotation_quaternion = new Quaternion(cos_theta, axis_vector.multiply(sin_theta));
-			var temp = rotation_quaternion.calculate_rotation_matrix();
-			matrix = multiply_matrix(temp, matrix);
-			old_pos.set_pos(x, y, z);
-		}
-	}
+function mousemove(e) {
+  if (isdraging) {
+    x = e.layerX - width / 2;
+    y = e.layerY - height / 2;
+    z = Math.sqrt((width * width) / 4 - x * x - y * y);
+    new_pos.set_pos(x, y, z);
+
+    //rotation part here
+    var cp = old_pos.cross_product(new_pos);
+    if (cp.x != 0) {
+      axis_vector = cp.nomalize();
+      rotation_quaternion = new Quaternion(
+        cos_theta,
+        axis_vector.multiply(sin_theta)
+      );
+      var temp = rotation_quaternion.calculate_rotation_matrix();
+      matrix = multiply_matrix(temp, matrix);
+      old_pos.set_pos(x, y, z);
+    }
+  }
 }
 
-function mouseup(e){
-	isdraging = false;
+function mouseup(e) {
+  isdraging = false;
 }
